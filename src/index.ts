24bb81912feb09e58d020f11f26f1ffc30c740fc
cotto89 <cottto89@gmail.com>
2017-01-29 12:49:48 +0900

    chore(package): add react-redux
    close(#2)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
 import './lib/polyfill/object';
+import { createElement as $ } from 'react';
+import { render } from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import createStore from './lib/flux/createStore';
+
+const state = {
+    count: 0
+};
+
+const store = createStore<{ count: number }, { up: number }>(state, {
+    'up': [(state: any, count: number) => ({ count: state.count + count })]
+});
+
+const Counter = (props: { count: number, countUp: Function }) => {
+    return $('div', {},
+        $('div', {}, props.count),
+        $('button', { onClick: () => props.countUp(1) }, 'up')
+    );
+};
+
+const mapStatetoProps = (s: { count: number }) => s;
+const mapDispatchtoProps = (dispath: typeof store.dispatch) => ({
+    countUp: (count: number) => {
+        dispath('up', count);
+    }
+});
+
+const App = connect(mapStatetoProps, mapDispatchtoProps)(Counter);
+
+window.addEventListener('DOMContentLoaded', () => {
+    render($(Provider, { store: store as any }, $(App)), document.getElementById('root'));
+});
