b657f7c2086416ed5044428030882fb9ea32fe0d
cotto89 <cottto89@gmail.com>
2017-01-31 23:22:51 +0900

    feat(mutation): add mutation
    - data.addProject
    - data.updateProject
    - data.deleteProject

--- src/reducers/data.ts
+++ src/reducers/data.ts
@@ -1,3 +1,5 @@
+import omit = require('lodash/omit');
+
 export interface DataState {
     projects: { [projectId: string]: Model.Project };
     topics: { [topicId: string]: Model.Topic };
@@ -14,3 +16,28 @@ export function initialDataState(): DataState {
 
 /* Reducer
 ----------------------- */
+type S = AppState;
+type DS = DataState;
+type PJ = Model.Project;
+
+/**
+ * Projectを追加
+ */
+export const addProject = (s: S, p: PJ) => ({
+    projects: { ...s.projects, [p.id]: p }
+});
+
+/**
+ * Projectを更新
+ */
+export const updateProject = addProject;
+
+/**
+ * Projectを削除
+ * projectが持つtopicIdsとpostIdsから関連itemも削除
+ */
+export const deleteProject = (s: S, p: PJ): Partial<S> => ({
+    projects: omit<DS['projects'], DS['projects']>(s.projects, p.id),
+    topics: omit<DS['topics'], DS['topics']>(s.topics, [...p.topicIds]),
+    posts: omit<DS['posts'], DS['posts']>(s.posts, [...p.postIds])
+});
