7df7e9a5d526e74f6fad6b26f73964444e147f78
cotto89 <cottto89@gmail.com>
2017-01-31 23:24:25 +0900

    feat(mutation): add mutation
    - session.setCurrentProjectId
    - session.setCurrentTopicId

--- src/reducers/session.ts
+++ src/reducers/session.ts
@@ -14,10 +14,29 @@ export function initialSessoinState() {
     };
 }
 
-export const updateCurrentIds = (state: AppState, route: Model.Route) => ({
+type S = AppState;
+type PJ = Model.Project;
+type T = Model.Topic;
+
+export const updateCurrentIds = (state: S, route: Model.Route) => ({
     session: {
         ...state.session,
         currentProjectId: route.params['projectId'],
         currentTopicId: route.params['topicId'],
     }
 });
+
+export const setCurrentProjectId = (s: S, p: PJ) => ({
+    session: {
+        ...s.session,
+        currentProjectId: p.id
+    }
+});
+
+export const setCurrentTopicId = (s: S, t: T) => ({
+    session: {
+        ...s.session,
+        currentTopicId: t.id
+    }
+});
+
