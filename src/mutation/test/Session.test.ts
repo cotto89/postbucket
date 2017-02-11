d5999ba85842895844c7354e832e211b647e5fa8
cotto89 <cottto89@gmail.com>
2017-02-11 10:41:38 +0900

    fix(mutation/Session): rewrite
    - remove mobx
    
    #17, #18

--- src/mutation/test/Session.test.ts
+++ src/mutation/test/Session.test.ts
@@ -1,14 +1,31 @@
 import { createElement } from 'react';
 import * as assert from 'assert';
-import AppStore from './../../app/store';
-import { Session } from '../index';
+import { initialState } from './../../app/state';
+import fixture from './../../app/helper/createProjectData';
+import { Session } from './../Session';
 
-let s: AppStore;
+let s: IAppState;
+let target: {
+    pj: IEntity.IProject,
+    t: IEntity.ITopic,
+    p: IEntity.IPost
+};
+
 beforeEach(() => {
-    s = new AppStore();
+    const projects = fixture({
+        projectCount: 2,
+        topicCountPerProject: 2,
+        postCountPerTopic: 2
+    });
+
+    s = initialState({ projects });
+    const [pj] = Object.values(s.projects);
+    const [t] = Object.values(pj.topics);
+    const [p] = Object.values(t.posts);
+    target = { pj, t, p };
 });
 
-const createRoute = (props: Partial<Model.IRoute>): Model.IRoute => ({
+const createRoute = (props: Partial<IEntity.IRoute>): IEntity.IRoute => ({
     component: () => createElement('div'),
     params: {},
     query: {},
@@ -17,43 +34,57 @@ const createRoute = (props: Partial<Model.IRoute>): Model.IRoute => ({
 });
 
 describe('.updateCurrentIds()', () => {
-    let route: Model.IRoute;
-    beforeEach(() => {
-        route = createRoute({
-            params: {
-                projectId: '1',
-                topicId: '2'
-            }
-        });
-
-        Session.updateCurrentIds(s, route);
-    });
-
     context('route.paramsにprojectIdあった場合', () => {
-        it('currentProjecIdに追加される', () => {
-            assert.equal(s.session.currentProjectId, '1');
+        it('currentProjecIdが更新される', () => {
+            const route = createRoute({
+                params: {
+                    projectId: '1'
+                }
+            });
+
+            const {session} = Session.updateCurrentIds(s, route);
+            assert.deepEqual(session, {
+                currentProjectId: '1',
+                currentTopicId: undefined
+            });
         });
     });
-
 
     context('route.paramsにtopicIdがあった場合', () => {
-        it('currentTopicIdに追加される', () => {
-            assert.equal(s.session.currentTopicId, '2');
+        it('currentTopicIdとcurrentProjectIdが更新される', () => {
+            const route = createRoute({
+                params: {
+                    topicId: target.t.id
+                }
+            });
+
+            const { session } = Session.updateCurrentIds(s, route);
+
+            assert.deepEqual(session, {
+                currentProjectId: target.pj.id,
+                currentTopicId: target.t.id
+            });
         });
     });
 });
 
 describe('.setCurrentProjectIds', () => {
     it('currentProjectIdに追加されること', () => {
-        Session.setCurrentProjectId(s, { id: '1' } as Model.IProject);
-        assert.equal(s.session.currentProjectId, '1');
+        const {session} = Session.setCurrentProjectId(s, { id: '1' } as IEntity.IProject);
+        assert.deepEqual(session, {
+            currentProjectId: '1',
+            currentTopicId: undefined
+        });
     });
 });
 
 describe('.setCurrentTopicIds', () => {
     it('currentTopicIdに追加されること', () => {
-        Session.setCurrentTopicId(s, { id: '1' } as Model.ITopic);
-        assert.equal(s.session.currentTopicId, '1');
+        const {session} = Session.setCurrentTopicId(s, { id: '1', projectId: '2' } as IEntity.ITopic);
+        assert.deepEqual(session, {
+            currentProjectId: '2',
+            currentTopicId: '1'
+        });
     });
 });
 
