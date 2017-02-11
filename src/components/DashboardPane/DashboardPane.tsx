160cb8c224b29133ddb6ca46eb520f12c8214704
cotto89 <cottto89@gmail.com>
2017-02-11 22:22:10 +0900

    refactor(DashboardPane):
    - 無駄なcomponentのnestを排除
    - mapXXXToPropsにusecaseを配置して、componentを薄くした

--- src/components/DashboardPane/DashboardPane.tsx
+++ src/components/DashboardPane/DashboardPane.tsx
@@ -3,45 +3,75 @@ import { connect } from 'react-redux';
 import { Project, Session, UI } from './../../mutation/index';
 import abortIf from './../utils/abortTransaction';
 
-import ProjectForm from './ProjectFrom';
-import ProjectCardList from './ProjectCardList';
+/* Container
+------------------------ */
+const mapStateToProps = (store: IAppStoreFromProvider) => ({
+    projects: Object.values(store.projects),
+    editingCardIds: store.ui.editingProjectCardIds
+});
+
+const mapDispatchToProps = (usecase: UseCase) => {
+    const ctx = 'editingProjectCardIds';
+
+    return {
+        actions: {
+            addProject: usecase('PROJECT::ADD').use<IEntity.IProject>([
+                (_, pj) => abortIf(() => !!pj.name),
+                UI.removeEditingId(ctx),
+                Project.setProject
+            ]),
+
+            updateProject: usecase('PROJECT::UPDATE').use<IEntity.IProject>([
+                (_, pj) => abortIf(() => !!pj.name),
+                UI.removeEditingId(ctx),
+                Project.setProject,
+            ]),
+
+            deleteProject: usecase('PROJECT::DELETE').use<IEntity.IProject>([
+                UI.removeEditingId(ctx),
+                Project.deleteProject
+            ]),
+
+            onCardSelect: usecase('PROJECT::SELECT').use<IEntity.IProject>([
+                UI.clearEditingIds(ctx),
+                Session.setCurrentProjectId,
+            ]),
+
+            toggleCardView: usecase('PROJECT::TOGGLE_CARD').use<IEntity.IProject>([
+                UI.toggleEditingIds(ctx),
+            ])
+        }
+    };
+};
+
 
 /* DashBoradPane
 ----------------------------------- */
+import RenderCase from './../utils/RenderCase';
+import ProjectView from './ProjectView';
+import ProjectForm from './ProjectFrom';
+
+type ProjectAction = (pj: IEntity.IProject) => void;
+
 interface Props {
-    projects: IAppState['projects'];
-    usecase: UseCase;
+    projects: IEntity.IProject[];
     editingCardIds: string[];
+    actions: {
+        addProject: ProjectAction;
+        updateProject: ProjectAction;
+        deleteProject: ProjectAction;
+        onCardSelect: ProjectAction;
+        toggleCardView: ProjectAction;
+    };
 }
 
 export class DashBoradPane extends React.Component<Props, {}> {
-    addProject = this.props.usecase('PROJECT::ADD').use<IEntity.IProject>([
-        (_, pj) => abortIf(() => !!pj.name),
-        UI.removeEditingId('editingProjectCardIds'),
-        Project.setProject
-    ]);
-
-    updateProject = this.props.usecase('PROJECT::UPDATE').use<IEntity.IProject>([
-        (_, pj) => abortIf(() => !!pj.name),
-        UI.removeEditingId('editingProjectCardIds'),
-        Project.setProject,
-    ]);
-
-    deleteProject = this.props.usecase('PROJECT::DELETE').use<IEntity.IProject>([
-        UI.removeEditingId('editingProjectCardIds'),
-        Project.deleteProject
-    ]);
-
-    onCardSelect = this.props.usecase('PROJECT::SELECT').use<IEntity.IProject>([
-        UI.clearEditingIds('editingProjectCardIds'),
-        Session.setCurrentProjectId,
-    ]);
-
-    toggleCardView = this.props.usecase('PROJECT_CARD_TOGGLE').use<IEntity.IProject>([
-        UI.toggleEditingIds('editingProjectCardIds'),
-    ]);
+    get projects() {
+        return this.props.projects;
+    }
 
     render() {
+        const {actions} = this.props;
         return (
             <div>
                 <header>
@@ -50,33 +80,41 @@ export class DashBoradPane extends React.Component<Props, {}> {
                 <ProjectForm
                     project={{ name: '' } as IEntity.IProject}
                     isNew
-                    onSubmit={this.addProject}
+                    onSubmit={actions.addProject}
                 />
 
-                <ProjectCardList
-                    updateProject={this.updateProject}
-                    deleteProject={this.deleteProject}
-                    onCardSelect={this.onCardSelect}
-                    toggleCardView={this.toggleCardView}
-                    editingCardIds={this.props.editingCardIds}
-                    projects={this.props.projects}
-                />
+                {
+                    /* ProjectCardList
+                    --------------------- */
+                    this.projects.map(pj =>
+                        /* ProjectCard
+                       --------------------- */
+                        <div className='ProjectCard' key={pj.id}>
+                            <RenderCase cond={!this.props.editingCardIds.includes(pj.id)}>
+                                {/* ProjectView
+                                -------------------------- */}
+                                <ProjectView
+                                    project={pj}
+                                    deleteProject={actions.deleteProject}
+                                    toggleCardView={actions.toggleCardView}
+                                    onSelect={actions.onCardSelect}
+                                />
+
+                                {/* ProjectForm
+                                ---------------------*/}
+                                <ProjectForm
+                                    project={pj}
+                                    onSubmit={actions.updateProject}
+                                    onCancel={actions.toggleCardView}
+                                />
+                            </RenderCase>
+                        </div>
+                    )
+                }
             </div>
         );
     }
 }
 
-
-/* Container
------------------------- */
-const mapStateToProps = (store: IAppStoreFromProvider) => ({
-    projects: store.projects,
-    usecase: store.usecase,
-    editingCardIds: store.ui.editingProjectCardIds
-});
-
-const mapDispatchToProps = (usecase: UseCase) => ({
-    usecase
-});
 
 export default connect(mapStateToProps, mapDispatchToProps)(DashBoradPane);
