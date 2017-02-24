d6e396629f1a03a8e7818a6f03c8996afb4a4b79
cotttpan <cotttpan@gmail.com>
2017-02-24 17:46:40 +0900

    feat(enhancer): add notifier

---
+++ src/lib/enhancer/notifier.ts
@@ -0,0 +1,37 @@
+export interface Notification {
+    result: any;
+    name: string;
+    taskName: string;
+    state: any;
+    error?: Error;
+}
+
+export interface Listener {
+    (notification: Notification): any;
+}
+
+export default function notifier(...listeners: Listener[]) {
+    return function enhancer(name: string, task: Function & { _taskName: string }) {
+        return function enhancedTask(state: any, params: any) {
+            let result;
+            let error;
+
+            try {
+                result = task(state, params);
+            } catch (err) {
+                error = err;
+            }
+
+            const notification: Notification = {
+                result,
+                state,
+                error,
+                name,
+                taskName: task._taskName || task.name || 'anonymous'
+            };
+
+            listeners.forEach(f => f(notification));
+            return result;
+        };
+    };
+}
