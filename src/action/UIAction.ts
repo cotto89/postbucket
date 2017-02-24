760a527d49afa09d4980b2afe3944edc0b145a19
cotttpan <cotttpan@gmail.com>
2017-02-24 21:02:38 +0900

    refactor(UIAction): replace set/update

--- src/action/UIAction.ts
+++ src/action/UIAction.ts
@@ -1,4 +1,4 @@
-import set = require('lodash/fp/set');
+import { set, update } from './../utils/object';
 import * as u from './../utils/utils';
 
 type S = IAppState;
@@ -6,7 +6,7 @@ type S = IAppState;
 export class UIAction {
     setEditingId = (context: 'editingProjectCardIds' | 'editingTopicCardIds' | 'editingPostIds') => {
         const fn = <U extends { id: string }>(s: S, u: U) => {
-            return set(['ui', context], [...s.ui[context], u.id], s);
+            return set(s, ['ui', context], [...s.ui[context], u.id]);
         };
 
         return u.task(`set_${context}`, fn);
@@ -14,7 +14,7 @@ export class UIAction {
 
     removeEditingId = (context: 'editingProjectCardIds' | 'editingTopicCardIds' | 'editingPostIds') => {
         const fn = <U extends { id: string }>(s: S, u: U) => {
-            return set(['ui', context], s.ui[context].filter(id => id !== u.id), s);
+            return update(s, ['ui', context], (v) => v.filter(id => id !== u.id));
         };
 
         return u.task(`remove_${context}`, fn);
@@ -22,7 +22,7 @@ export class UIAction {
 
     clearEditingIds = (context: 'editingProjectCardIds' | 'editingTopicCardIds' | 'editingPostIds') => {
         const fn = <U extends { id: string }>(s: S, _u: U) => {
-            return set(['ui', context], [], s);
+            return set(s, ['ui', context], []);
         };
         return u.task(`clear_${context}`, fn);
     }
