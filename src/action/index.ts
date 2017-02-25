85377971af6a2271006d9115c1800874641873c4
cotttpan <cotttpan@gmail.com>
2017-02-25 14:02:14 +0900

    fix(Action): fix index.ts

--- src/action/index.ts
+++ src/action/index.ts
@@ -1,11 +1,13 @@
 import { UIAction } from './UIAction';
 import { SessionAction } from './SessionAction';
 import { ProjectAction } from './ProjectAction';
+import { TopicAction } from './TopicAction';
 import abortIf from './abortIf';
 
-export { UIAction, SessionAction, ProjectAction }
+export { UIAction, SessionAction, ProjectAction, TopicAction }
 export default {
     ui: new UIAction(),
+    topics: new TopicAction(),
     project: new ProjectAction(),
     session: new SessionAction(),
     abortIf
