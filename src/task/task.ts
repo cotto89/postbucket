1c59c7dd83d4cef9bf17ac12e4f48cfa1dbb66c2
cotttpan <cotttpan@gmail.com>
2017-03-05 17:18:54 +0900

    fix(*): 不要になった関数を削除

--- src/task/task.ts
+++
@@ -1,9 +0,0 @@
-/**
- * class propertyだとfunction.nameがなくなるので, taskに_taskNameを追加する
- */
-function task<T extends Function>(name: string, fn: T): T {
-    (fn as any)._taskName = name;
-    return fn;
-}
-
-export default task;
