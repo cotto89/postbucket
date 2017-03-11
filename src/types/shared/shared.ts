99e8a67eda1098c4028f8f106ddea726eb64b984
cotttpan <cotttpan@gmail.com>
2017-03-11 16:28:07 +0900

    chore(*): 環境復元

---
+++ src/types/shared/shared.ts
@@ -0,0 +1,22 @@
+// import * as E from './../../store/entity';
+// import * as S from './../../store/state';
+// import * as Store from './../../store/store';
+// import { PostBucketIDB, Model } from './../../idb/index';
+
+// export interface UseCase extends Store.UseCase { }
+
+// export interface IAppState extends S.IState { }
+
+// export namespace Entity {
+//     export interface IProject extends E.IProject { }
+//     export interface ITopic extends E.ITopic { }
+//     export interface IPost extends E.IPost { }
+//     export interface IRoute extends E.IRoute { }
+// }
+
+// export namespace IDB {
+//     export interface Instance extends PostBucketIDB { }
+//     export interface IProjectModel extends Model.IProjectModle { }
+//     export interface ITopicModel extends Model.ITopicModel { }
+//     export interface IPostModel extends Model.IPostModel { }
+// }
