3610268fc737b1da979521cb5e440f2ff2ac4aad
cotttpan <cotttpan@gmail.com>
2017-03-14 19:39:58 +0900

    feat(store): local storageによる永続化を追加

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,8 +4,16 @@ import { ActionTypes, IState } from '@shared';
 import { state as initialState } from './entity';
 import reducemap from './reducemap';
 
-const state = initialState();
-let store = createStore<IState, ActionTypes>(state, reducemap);
+const STORAGE_KEY = 'state';
+/*
+ * 初期load時にlocalStorageからstate(projectsとtopics)を取得する
+ * 初期レンダリング時に真っ白画面を出さないためにやってる
+ * https://github.com/cotttpan/postbucket/issues/26
+ */
+const state = (() => {
+    const json = localStorage.getItem(STORAGE_KEY);
+    return json ? initialState(JSON.parse(json)) : initialState();
+})();
 
 const middlewares: any[] = [];
 if (process.env.NODE_ENV === 'development') {
@@ -13,4 +21,20 @@ if (process.env.NODE_ENV === 'development') {
     middlewares.push(reduxDevtools(state));
 }
 
-export default (applyMiddleware(store as any, middlewares) as any) as typeof store;
+let store = createStore<IState, ActionTypes>(state, reducemap);
+store = (applyMiddleware(store as any, middlewares) as any) as typeof store;
+
+/*
+ * localStorageにsession以外のstateを投げる
+ * sessionはrouting依存なので避けている
+ */
+store.subscribe(() => {
+    const s = store.getState();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({
+        categories: s.categories,
+        topics: s.topics,
+        posts: s.posts
+    }));
+});
+
+export default store;
