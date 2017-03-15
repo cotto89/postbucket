098f185cec572956cb4187235f0536128b9514a0
cotttpan <cotttpan@gmail.com>
2017-03-15 20:34:51 +0900

    chore(*)

--- temp/_src/components/container/container.ts
+++
@@ -1,4 +0,0 @@
-export { default as TopicListContainer } from './../TopicListPane/TopicListPane'
-export { default as PostListContainer } from './../PostListPane/PostListPane'
-export { default as EditorContainer } from './../EditorPane/EditorPane'
-
--- temp/_src/lib/quex-utils/combineEnhancer.ts
+++
@@ -1,8 +0,0 @@
-import * as Types from '@shared';
-import { Enhancer } from 'quex';
-export default function combineEnhancer(enhancers: Function[]) {
-    return enhancer as Enhancer<Types.IAppState>;
-    function enhancer(name: string, task: Function) {
-        return enhancers.reduce((enhancedTask, enhancer) => enhancer(name, enhancedTask), task);
-    };
-}
