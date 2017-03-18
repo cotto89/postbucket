1af9256997de0ddb1b028026d88cff10cea8aafa
cotttpan <cotttpan@gmail.com>
2017-03-18 15:01:10 +0900

    chore(utils): remove utils/object
    - lodash wrapperを削除
    - 代わりにiohを導入

--- src/store/reducemap.ts
+++ src/store/reducemap.ts
@@ -1,31 +1,28 @@
 import { ReduceMap } from './../lib/flux/createStore';
-import { set, update, omit } from './../utils/object';
+import { setIn, deleteIn } from 'ioh';
 import { ActionTypes, IState } from '@shared';
 
 const reducemap: ReduceMap<IState, ActionTypes> = {
-    'CATEGORY:ADD': (s, c) => set(s, ['categories', String(c.id)], c),
-    'CATEGORY:UPDATE': (s, c) => set(s, ['categories', String(c.id)], c),
-    'CATEGORY:DELETE': (s, c) => update(s, ['categories'], (o) => omit(o, String(c.id))),
+    'CATEGORY:ADD': (s, c) => setIn(s, ['categories', String(c.id)], c),
+    'CATEGORY:UPDATE': (s, c) => setIn(s, ['categories', String(c.id)], c),
+    'CATEGORY:DELETE': (s, c) => deleteIn(s, ['categories', String(c.id)]) as IState,
 
-    'TOPIC:ADD': (s, t) => set(s, ['topics', String(t.id)], t),
-    'TOPIC:UPDATE': (s, t) => set(s, ['topics', String(t.id)], t),
-    'TOPIC:DELETE': (s, t) => update(s, ['topics'], o => omit(o, String(t.id))),
+    'TOPIC:ADD': (s, t) => setIn(s, ['topics', String(t.id)], t),
+    'TOPIC:UPDATE': (s, t) => setIn(s, ['topics', String(t.id)], t),
+    'TOPIC:DELETE': (s, t) => deleteIn(s, ['topics', String(t.id)]) as IState,
 
-    'POST:ADD': (s, p) => set(s, ['posts', String(p.id)], p),
-    'POST:UPDATE': (s, p) => set(s, ['posts', String(p.id)], p),
-    'POST:DELETE': (s, p) => update(s, ['posts'], o => omit(o, String(p.id))),
+    'POST:ADD': (s, p) => setIn(s, ['posts', String(p.id)], p),
+    'POST:UPDATE': (s, p) => setIn(s, ['posts', String(p.id)], p),
+    'POST:DELETE': (s, p) => deleteIn(s, ['posts', String(p.id)]) as IState,
 
-    'SESSION:UPDATE_BY_ROUTE': (s, r) => set(s, ['session'], {
+    'SESSION:UPDATE_BY_ROUTE': (s, r) => setIn(s, ['session'], {
         ...s.session,
         currentCategoryId: r.params['categoryId'] || r.query['categoryId'] || undefined,
         currentTopicId: r.params['topicId'] || r.query['topicId'] || undefined,
         currentPostId: r.params['postId'] || r.query['postId'] || undefined
     } as IState['session']),
 
-    'SESSION:SET_EDITING_TOPIC_ID': (s, t) => set(s, ['session'], {
-        ...s.session,
-        editingTopicId: t.id
-    }),
+    'SESSION:SET_EDITING_TOPIC_ID': (s, t) => setIn(s, ['session', 'editingTopicId'], t.id),
 
     'STATE:SET_STATE': (s1, s2) => ({ ...s1, ...s2 }),
 };
